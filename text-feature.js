a98775a1bf820ea48c20254cb0de7c625371f857
ebody <e-body@arcor.de>
2021-03-11 12:23:07 +0100

    Im Masterbranch eine neue Zeile in text-feature.js hinzugefügt

--- text-feature.js
+++ text-feature.js
@@ -1,3 +1,4 @@
 // Diese Zeile sollte im Master erscheinen
 // Diese Zeile sollte im Bugfix Branch erscheinen
 // Diese Zeile habe ich im Feature Branch hinzugefügt
+// eine neue Änderung im Master Branch, nachdem der Bugfix Branch erstellt wurde
